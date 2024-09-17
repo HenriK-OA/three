6ef901f1242e6d773eaf252e9773be2a4337c036
ordiAxro <henri.kivi.sy@gmail.com>
2024-09-18 01:12:55 +0300

    Logo

--- ordiAxro.js
+++ ordiAxro.js
@@ -1,23 +1,24 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { mod } from 'three/webgpu';
+
+
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
+const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({antialias:true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// Add ambient light
-const directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
-scene.add( directionalLight );
-directionalLight.position.set(-1,1,1)
-const light = new THREE.AmbientLight(0xffffff, 0.2); // soft white light
-scene.add(light);
 
-// Set camera position
+const directionalLight = new THREE.DirectionalLight( 0xffffff, 100 );
+scene.add( directionalLight );
+directionalLight.position.set(-1,-0.5,1)
+const light = new THREE.AmbientLight(0xffffff, 0.2);
+scene.add(light);
 camera.position.z = 2.3;
 const loader = new GLTFLoader();
 let model;
+
+
 loader.load(
 	'ordiAxro.glb',
 
@@ -25,32 +26,26 @@ loader.load(
         model = gltf.scene;
         model.traverse((object) => {
             if (object.isMesh) {
-                // Replace the material with a custom one
                 object.material = new THREE.MeshStandardMaterial({
-                    color: 0x00ff00, // Red color for the mesh
-                    metalness: 0.5,
-                    roughness: 0.5
+                    color: 0x00ff00, 
+                    metalness: 1,
+                    roughness: 0.1
                 });
             }
         });
-
         scene.add(model);
         animate();
 	},
-	// called while loading is progressing
 	function ( xhr ) {
-
 		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-
 	},
-	// called when loading has errors
 	function ( error ) {
-
 		console.log( 'An error happened' );
-
 	}
     
 );
+
+
 function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
@@ -58,3 +53,10 @@ function animate() {
         model.rotation.y +=0.005;
     }
 }
+
+
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
